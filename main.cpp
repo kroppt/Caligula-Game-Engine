459516913b799a2d7bd512c1e4495d7ff443c877
Tyler Kropp <kroppt@purdue.edu>
2018-01-18 16:38:27 -0500

    Add SDL code for window creation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include <stdio.h>
+#include <iostream>
 #include <SDL.h>
 #include "audio/AudioInterface.h"
 
 int main(int argc, char** argv){
     initAudio();
-    SDL_Init(SDL_INIT_EVERYTHING);
-    printf("Hello World\n");
+    // Initialize video only to avoid failure
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    int res_x = 1280;
+    int res_y = 720;
+    SDL_Window *win = SDL_CreateWindow(
+        "Test Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        res_x,
+        res_y,
+        SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        std::cout << "SDL_CreateWindow error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    // Wait 3 seconds
+    SDL_Delay(3000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    std::cout << "Reached end of file successfully." << std::endl;
     return 0;
 }
