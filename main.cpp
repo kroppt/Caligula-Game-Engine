a5fd9d27077ae162b6c3d6841d0b2c665a54669a
Nicholas Nusgart <nicknusgart@gmail.com>
2018-01-18 16:25:17 -0600

    Start of OpenGL: Initialize context

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 #include "audio/AudioInterface.h"
 
 int main(int argc, char** argv){
     initAudio();
     // Initialize video only to avoid failure
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER|SDL_INIT_EVENTS) != 0) {
         std::cout << "SDL_Init error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
     int res_x = 1280;
     int res_y = 720;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+
+    /* Turn on double buffering with a 24bit Z buffer.
+     * You may need to change this to 16 or 32 for your system */
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     SDL_Window *win = SDL_CreateWindow(
         "Test Window",
         SDL_WINDOWPOS_CENTERED,
@@ -24,6 +34,10 @@ int main(int argc, char** argv){
         SDL_Quit();
         return 1;
     }
+    
+    SDL_GLContext context = SDL_GL_CreateContext(win);
+    SDL_GL_SetSwapInterval(1);
+
     // Wait 3 seconds
     SDL_Delay(3000);
     SDL_DestroyWindow(win);
