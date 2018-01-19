b405b018732e7e0289ac887dd250a26d352bea67
tkdestroyer1 <tkropp@purdue.edu>
2018-01-18 20:50:33 -0500

    Add texture loading skeleton

---
+++ src/texture.cpp
@@ -0,0 +1,20 @@
+#include "texture.h"
+
+using namespace std;
+
+TextureLoader::TextureLoader() {
+}
+
+TextureLoader::TextureLoader(const string) {
+}
+
+void TextureLoader::setFile(const string) {
+}
+
+int TextureLoader::readFile() {
+    return 0;
+}
+
+void **TextureLoader::getBuffer() {
+    return nullptr;
+}
