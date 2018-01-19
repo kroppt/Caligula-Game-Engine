35951325500d1bd3395e780255238feea1930b4b
Nicholas Nusgart <nicknusgart@gmail.com>
2018-01-18 20:09:03 -0600

    Fix OpenGL and Audio to not crash

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <glad/glad.h>
-#include <GL/glx.h>
 #include <SDL2/SDL_opengl.h>
 #include "audio/AudioInterface.h"
 #include "utils.h"
@@ -18,16 +17,16 @@ int main(int argc, char** argv){
         return 1;
     }
     printf("Initialized SDL\n");
-    int res_x = 1280;
-    int res_y = 720;
+    int res_x = 1920;
+    int res_y = 1080;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
     /* Turn on double buffering with a 24bit Z buffer.
      * You may need to change this to 16 or 32 for your system */
-    //SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    //SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
     SDL_Window *win = SDL_CreateWindow(
         "Test Window",
@@ -35,21 +34,20 @@ int main(int argc, char** argv){
         SDL_WINDOWPOS_CENTERED,
         res_x,
         res_y,
-        SDL_WINDOW_SHOWN);
+        SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL);
     if (win == NULL) {
-        std::cout << "SDL_CreateWindow error: " << SDL_GetError() << std::endl;
+        printf("SDL_CreateWindow error %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
     printf("Created Window\n");
     SDL_GLContext context = SDL_GL_CreateContext(win);
+    if(!context){
+        printf("SDL_GL_CreateContext failed %s\n", SDL_GetError());
+    }
     SDL_GL_MakeCurrent(win, context);
     SDL_GL_SetSwapInterval(1);
     if (!gladLoadGL()) {
-        GLXContext glx = glXGetCurrentContext();
-        if(!glx){
-            printf("NO GLX CONTEXT!\n");
-        }
         std::cout << "gladLoadGL error" << std::endl;
         SDL_Quit();
         return 1;
@@ -60,6 +58,6 @@ int main(int argc, char** argv){
     SDL_DestroyWindow(win);
     SDL_Quit();
     std::cout << "Reached end of file successfully." << std::endl;
-    
+    quitAudio();
     return 0;
 }
