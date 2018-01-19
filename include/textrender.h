dba6bd351be61782374d913a0f75e085ddc6ceb0
Tyler Kropp <kroppt@purdue.edu>
2018-01-19 18:16:54 -0500

    Add text renderer prototype

---
+++ include/textrender.h
@@ -0,0 +1,22 @@
+#include <harfbuzz/hb-ft.h>
+#include <string>
+
+const int FONTSIZE_SMALL = 24;
+const int FONTSIZE_MEDIUM = 48;
+const int FONTSIZE_LARGE = 72;
+const int FONTSIZE_DEFAULT = FONTSIZE_MEDIUM;
+
+class TextRender {
+    public:
+        TextRender();
+        ~TextRender();
+        TextRender(const std::string, const double);
+        void ChangeFont(const std::string, const double);
+        int WriteText(const char *);
+    private:
+        int font_size;
+        std::string font_name;
+        hb_font_t * curr_font;
+        FT_Face ft_face;
+        FT_Library ft_library;
+};
